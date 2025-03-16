d336062f3e0dd911d7009c978ea2fe59d8aef397
BakangMonei <bakangmonei2@gmail.com>
2025-03-16 09:13:04 +0200

    Now going to VScode:Cursor

--- src/main/java/com/moneibakang/informationretrievalbackend/util/AnalyzerFactory.java
+++ src/main/java/com/moneibakang/informationretrievalbackend/util/AnalyzerFactory.java
@@ -1,10 +1,10 @@
 package com.moneibakang.informationretrievalbackend.util;
 
 /*
- * @Author: Monei Bakang
- * @Date: 16 March 2025
- * @Time: 07:24 hours
- */
+* @Author: Monei Bakang
+* @Date: 16 March 2025
+* @Time: 07:24 hours
+*/
 
 public class AnalyzerFactory {
 }
